0076ccd9751b1b6e5bc073231c5fa701ce72816c
garfias06 <garfias06_23@outlook.com>
2023-04-17 22:56:53 -0500

    webapp-working-better

--- README.md
+++ README.md
@@ -1,1 +1,49 @@
-# weather-forecast
+# Weather Forecast
+
+## Description
+Server-side APIs (Application Programming Interface) are applications used by developers to increase development speed helping integrating new software to existing apps facilitating the user a better experience in a shorter amount of time.
+
+OpenWeather is one of many open source APIs who facilitate information to develop web applications. In this case we were asked to create a weather-forecast application.
+
+## Usage
+When web page is displayed, the traveler (user) will be presented with a search section on the left side of the page so any city within the US can be found and weather will be shown to the traveler.
+
+![city1](./assets/images/city1.jpg)
+
+The weather information that this webpage displays to the user is the current weather of the day searched plus information about the weather in the next five days.
+
+The information that is presented to the user is the following:
+- City Name
+- Date
+- Main Temperature
+- Humidity
+- Wind Speed
+
+![city2](./assets/images/city2.jpg)
+
+## Credits
+- https://openweathermap.org/
+- https://www.w3schools.com/
+
+## License
+MIT License
+
+Copyright (c) [2023] [Ulises-Garfias]
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
--- assets/script.js
+++ assets/script.js
@@ -1,16 +1,16 @@
 
-
+// FORM Variables
 var searchCity = document.querySelector(".search-city");
 var inputCity = document.querySelector("#city");
 var searchBtn = document.querySelector("#search");
 var searchHistory = document.querySelector(".search-history");
-// var api="9c797f3ee7047a8ba0a153885817289f"
+var api="9c797f3ee7047a8ba0a153885817289f"
 
 
 var city=[];
 
 function cityList(){
-    // searchHistory.innerHTML="";
+    searchHistory.innerHTML="";
 
     for (let i = 0; i < city.length; i++) {
         var cities = city[i];
@@ -18,23 +18,15 @@ function cityList(){
         var button=document.createElement("button");
         button.textContent=cities;
         button.setAttribute("data-index", i);
+        button.classList.add("list-button")
 
         searchHistory.appendChild(button);
     }
 }
 
-function history(){
-    var storedCity=JSON.parse(localStorage.getItem("city")); 
-    if (storedCity !== null) {
-        city = storedCity;
-      }
-      cityList();
-}
-
 function storeCity(){
     localStorage.setItem("City", JSON.stringify(city));
 }
-
 
 searchCity.addEventListener("submit", function(event){
     event.preventDefault();
@@ -44,167 +36,129 @@ searchCity.addEventListener("submit", function(event){
     }
     city.push(cityText);
     inputCity.value="";
+    city.reverse();
+
+    var storedCity=JSON.parse(localStorage.getItem("city")); 
+    if (storedCity !== null) {
+        city = storedCity;
+      }
     storeCity();
     cityList();
 })
 
-searchHistory.addEventListener("click", function(event){
-    var element=event.target;
+// Current Weather Data
 
-    if(element.matches("button")===true){
-        var index=element.parentElement.getAttribute("data-index");
-        city.splice(index, 1);
-        storeCity();
-        cityList();
-    }
+searchCity.addEventListener("submit", function(){
+
+var requestCurrentWeather='https://api.openweathermap.org/data/2.5/weather?q='+city+',USA&units=imperial&appid='+api;
+
+fetch(requestCurrentWeather)
+.then(function (response) {
+  return response.json();
 })
-history();
+
+.then(function (data) {
+    console.log(data)
+
+var mainCity=document.querySelector(".main-city");
+var mainDate=document.querySelector(".main-date");
+var mainTemp=document.querySelector(".main-temp");
+var mainWind=document.querySelector(".main-wind");
+var mainHumidity=document.querySelector(".main-humidity");
+
+mainCity.textContent="City: " + data.name;
+mainDate.textContent="Date: " + data.timezone;
+mainTemp.textContent="Temp: " + data.main.temp + " °";
+mainWind.textContent="Wind: " + data.wind.speed + " miles/hour";
+mainHumidity.textContent="Humidity: " + data.main.humidity + "%";
+
+});
 
 
+// 5day Weather Data
+
+var request5dayWeather = 'https://api.openweathermap.org/data/2.5/forecast?q='+city+',USA&units=imperial&appid='+api;
+
+fetch(request5dayWeather)
+    .then(function (response) {
+        return response.json();
+    })
+    // DAY 1:
+
+    .then(function (data) {
+        console.log(data)
+
+        // DAY 1:
+
+        console.log(data.list[3]);
+
+        var date1 = document.querySelector(".date1");
+        var temp1 = document.querySelector(".temp1");
+        var wind1 = document.querySelector(".wind1");
+        var humidity1 = document.querySelector(".humidity1");
+
+        date1.textContent = "Date: " + data.list[3].dt_txt;
+        temp1.textContent = "Temp: " + data.list[3].main.temp + " °";
+        wind1.textContent = "Wind: " + data.list[3].wind.speed + " miles/hour";
+        humidity1.textContent = "Humidity: " + data.list[3].main.humidity + "%";
 
 
+        // DAY 2:
+
+        console.log(data.list[11]);
+
+        var date2 = document.querySelector(".date2");
+        var temp2 = document.querySelector(".temp2");
+        var wind2 = document.querySelector(".wind2");
+        var humidity2 = document.querySelector(".humidity2");
+
+        date2.textContent = "Date: " + data.list[11].dt_txt;
+        temp2.textContent = "Temp: " + data.list[11].main.temp + " °";
+        wind2.textContent = "Wind: " + data.list[11].wind.speed + " miles/hour";
+        humidity2.textContent = "Humidity: " + data.list[11].main.humidity + "%";
+
+        // DAY 3:
+
+        console.log(data.list[19]);
+
+        var date3 = document.querySelector(".date3");
+        var temp3 = document.querySelector(".temp3");
+        var wind3 = document.querySelector(".wind3");
+        var humidity3 = document.querySelector(".humidity3");
+
+        date3.textContent = "Date: " + data.list[19].dt_txt;
+        temp3.textContent = "Temp: " + data.list[19].main.temp + " °";
+        wind3.textContent = "Wind: " + data.list[19].wind.speed + " miles/hour";
+        humidity3.textContent = "Humidity: " + data.list[19].main.humidity + "%";
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// // Current Weather Data
-
-// var requestCurrentWeather='https://api.openweathermap.org/data/2.5/weather?q=USA&units=imperial&appid='+api;
-
-// fetch(requestCurrentWeather)
-// .then(function (response) {
-//   return response.json();
-// })
-
-// .then(function (data) {
-//     console.log(data)
-
-
-// var mainCity=document.querySelector(".main-city");
-// var mainDate=document.querySelector(".main-date");
-// var mainTemp=document.querySelector(".main-temp");
-// var mainWind=document.querySelector(".main-wind");
-// var mainHumidity=document.querySelector(".main-humidity");
-
-// mainCity.textContent="City: " + data.name;
-// mainDate.textContent="Date: " + data.timezone;
-// mainTemp.textContent="Temp: " + data.main.temp + " °";
-// mainWind.textContent="Wind: " + data.wind.speed + " miles/hour";
-// mainHumidity.textContent="Humidity: " + data.main.humidity + "%";
-
-// });
-
-// // 5day Weather Data
-
-// var request5dayWeather = 'https://api.openweathermap.org/data/2.5/forecast?q=USA&units=imperial&appid='+api;
-
-// fetch(request5dayWeather)
-//     .then(function (response) {
-//         return response.json();
-//     })
-
-//     // .then(function (data) {
-//     //     console.log(data)
-//     // })
-//     // DAY 1:
-
-//     .then(function (data) {
-//         console.log(data)
-
-//         // DAY 1:
-
-//         console.log(data.list[3]);
-
-//         var date1 = document.querySelector(".date1");
-//         var temp1 = document.querySelector(".temp1");
-//         var wind1 = document.querySelector(".wind1");
-//         var humidity1 = document.querySelector(".humidity1");
-
-//         date1.textContent = "Date: " + data.list[3].dt_txt;
-//         temp1.textContent = "Temp: " + data.list[3].main.temp + " °";
-//         wind1.textContent = "Wind: " + data.list[3].wind.speed + " miles/hour";
-//         humidity1.textContent = "Humidity: " + data.list[3].main.humidity + "%";
-
-
-//         // DAY 2:
-
-//         console.log(data.list[11]);
-
-//         var date2 = document.querySelector(".date2");
-//         var temp2 = document.querySelector(".temp2");
-//         var wind2 = document.querySelector(".wind2");
-//         var humidity2 = document.querySelector(".humidity2");
-
-//         date2.textContent = "Date: " + data.list[11].dt_txt;
-//         temp2.textContent = "Temp: " + data.list[11].main.temp + " °";
-//         wind2.textContent = "Wind: " + data.list[11].wind.speed + " miles/hour";
-//         humidity2.textContent = "Humidity: " + data.list[11].main.humidity + "%";
-
-//         // DAY 3:
-
-//         console.log(data.list[19]);
-
-//         var date3 = document.querySelector(".date3");
-//         var temp3 = document.querySelector(".temp3");
-//         var wind3 = document.querySelector(".wind3");
-//         var humidity3 = document.querySelector(".humidity3");
-
-//         date3.textContent = "Date: " + data.list[19].dt_txt;
-//         temp3.textContent = "Temp: " + data.list[19].main.temp + " °";
-//         wind3.textContent = "Wind: " + data.list[19].wind.speed + " miles/hour";
-//         humidity3.textContent = "Humidity: " + data.list[19].main.humidity + "%";
-
-
-//         // DAY 4
+        // DAY 4
         
-//         console.log(data.list[27]);
+        console.log(data.list[27]);
 
-//         var date4 = document.querySelector(".date4");
-//         var temp4 = document.querySelector(".temp4");
-//         var wind4 = document.querySelector(".wind4");
-//         var humidity4 = document.querySelector(".humidity4");
+        var date4 = document.querySelector(".date4");
+        var temp4 = document.querySelector(".temp4");
+        var wind4 = document.querySelector(".wind4");
+        var humidity4 = document.querySelector(".humidity4");
     
-//         date4.textContent = "Date: " + data.list[27].dt_txt;
-//         temp4.textContent = "Temp: " + data.list[27].main.temp + " °";
-//         wind4.textContent = "Wind: " + data.list[27].wind.speed + " miles/hour";
-//         humidity4.textContent = "Humidity: " + data.list[27].main.humidity + "%";
+        date4.textContent = "Date: " + data.list[27].dt_txt;
+        temp4.textContent = "Temp: " + data.list[27].main.temp + " °";
+        wind4.textContent = "Wind: " + data.list[27].wind.speed + " miles/hour";
+        humidity4.textContent = "Humidity: " + data.list[27].main.humidity + "%";
 
-//         // DAY 5
+        // DAY 5
 
-//         console.log(data.list[35]);
+        console.log(data.list[35]);
 
-//         var date5 = document.querySelector(".date5");
-//         var temp5 = document.querySelector(".temp5");
-//         var wind5 = document.querySelector(".wind5");
-//         var humidity5 = document.querySelector(".humidity5");
+        var date5 = document.querySelector(".date5");
+        var temp5 = document.querySelector(".temp5");
+        var wind5 = document.querySelector(".wind5");
+        var humidity5 = document.querySelector(".humidity5");
 
-//         date5.textContent = "Date: " + data.list[35].dt_txt;
-//         temp5.textContent = "Temp: " + data.list[35].main.temp + " °";
-//         wind5.textContent = "Wind: " + data.list[35].wind.speed + " miles/hour";
-//         humidity5.textContent = "Humidity: " + data.list[35].main.humidity + "%";
+        date5.textContent = "Date: " + data.list[35].dt_txt;
+        temp5.textContent = "Temp: " + data.list[35].main.temp + " °";
+        wind5.textContent = "Wind: " + data.list[35].wind.speed + " miles/hour";
+        humidity5.textContent = "Humidity: " + data.list[35].main.humidity + "%";
 
-//     });
+    });
+})
